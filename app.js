b6df4660679add2e63e4bc32de6abc0dc34e57f2
Preethi Mohan <preethi.mohan@in.ibm.com>
2017-02-15 19:06:53 +0530

    Created the asset registration page

--- app.js
+++ app.js
@@ -4,25 +4,34 @@
 // node.js starter application for Bluemix
 //------------------------------------------------------------------------------
 
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
+var express = require('express'),
+http = require('http');
 
 // cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
-
+app.engine('html', require('ejs').renderFile);
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+//Handle the form parameters
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
+//Create new asset
+app.post('/newAsset', function(request, response) {
+	console.log('Creating new asset : ' + JSON.stringify(request.body));
+	response.redirect('message.html');
+});
+
+//get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
